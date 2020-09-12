a1e6eeb6081627cb36febce4e2dca4ccca8c122c
Ohranj <ajdorrington@hotmail.co.uk>
2020-09-12 20:55:13 +0100

    added heroku deployment setup code and ran build

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
         extended: true,
     })
 );
-app.use(express.static("./backend/uploads"));
+app.use(express.static(path.join("./backend/uploads")));
 app.use(
     cookieSession({
         maxAge: 1000 * 60 * 60 * 12,
@@ -42,7 +42,6 @@ app.use("/memory", modelToSearch, memoryRoutes);
 //Heroku hosting
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
-    app.use(express.static("./backend/uploads"));
     app.get("*", (req, res) => {
         res.sendFile(
             path.resolve(__dirname, "frontend", "build", "index.html")
