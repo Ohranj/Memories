4db7d289e836341b4cebcf4c2b57e4780a7bf12c
Ohranj <ajdorrington@hotmail.co.uk>
2020-09-12 15:52:07 +0100

    added heroku deployment setup code and ran build

--- server.js
+++ server.js
@@ -38,9 +38,10 @@ app.use("/register", registerRoutes);
 app.use("/account", modelToSearch, userRoutes);
 app.use("/memory", modelToSearch, memoryRoutes);
 
-//Heroku deployment build
+//Heroku deployment
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("frontend/build"));
+    app.use(express.static("./backend/uploads"));
     app.get("*", (req, res) => {
         res.sendFile(
             path.resolve(__dirname, "frontend", "build", "index.html")
