80b76e0119d4078de01d9b08754376d904b1ddcb
Ohranj <ajdorrington@hotmail.co.uk>
2020-09-12 20:58:28 +0100

    added heroku deployment setup code and ran build

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(
         extended: true,
     })
 );
-app.use(express.static(path.join("./backend/uploads")));
+app.use(express.static(path.join(__dirname, "./backend/uploads")));
 app.use(
     cookieSession({
         maxAge: 1000 * 60 * 60 * 12,
