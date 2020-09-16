52ce64f4a39af4cfd2f1a41344debd9750dadaf8
Ohranj <ajdorrington@hotmail.co.uk>
2020-09-16 10:54:13 +0100

    Emails now send to local registered users and their additional email contacts

--- backend/services/nodemailer.js
+++ backend/services/nodemailer.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer");
 const cron = require("node-cron");
 
+const LocalModel = require("../models/LocalUser");
+
 (async () => {
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -12,14 +14,52 @@ const cron = require("node-cron");
         },
     });
 
-    cron.schedule("30 7 * * *", () => {
-        transporter.sendMail({
-            from: '"Memories" <ajdorrington18@gmail.com>',
-            to: "ajdorrington@hotmail.co.uk",
-            subject: "Daily Memory",
-            text: "Hello world?",
-            html: "<b>Hello world?</b>",
-        });
+    cron.schedule("30 7 * * *", async () => {
+        const test = await LocalModel.find({ notify: true }).exec();
+        const generateRandom = (memories) => {
+            return Math.floor(Math.random() * memories);
+        };
+        for (let doc of test) {
+            if (doc.memories.length > 0) {
+                const contactList = doc.additionalEmails.map((item) => {
+                    return item.email;
+                });
+                transporter.sendMail({
+                    from: '"Memories" <ajdorrington18@gmail.com>',
+                    to: [...contactList, doc.email],
+                    subject: "Daily Memory",
+                    text: "Daily Memory",
+                    html: `
+                        <h1>${
+                            doc.memories[generateRandom(doc.memories.length)]
+                                .title
+                        }</h1>
+                        <ul>
+                            <li>Scenario:- ${
+                                doc.memories[
+                                    generateRandom(doc.memories.length)
+                                ].scenario
+                            }</li>
+                            <li>Date:- ${
+                                doc.memories[
+                                    generateRandom(doc.memories.length)
+                                ].date
+                            }</li>
+                        </ul>
+                        <img src=${
+                            doc.memories[generateRandom(doc.memories.length)]
+                                .memoryImg
+                        } />
+                        <p>${
+                            doc.memories[generateRandom(doc.memories.length)]
+                                .blurb
+                        }</p>
+                    `,
+                });
+            } else {
+                console.log("No memories to send for contact");
+            }
+        }
     });
 })()
     .then(console.log("Emails sent daily at 7:30am"))
